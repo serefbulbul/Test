1996908e513c9941fbc18602a0bd5dc847fb58b6
Seref Bulbul <bulbulseref@gmail.com>
2023-11-01 15:47:47 +0100

    Initial commit

---
+++ Editor/SSOPreProcessBuild.cs
@@ -0,0 +1,128 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+
+namespace Com.Test.Core.Frontend.SingleSignOn
+{
+    [InitializeOnLoad]
+    public class SSOPreProcessBuild : IPreprocessBuildWithReport
+    {
+        private const string DefineSignInWithGoogle = "#define SIGN_IN_WITH_GOOGLE";
+        private const string DefineSignInWithApple = "#define SIGN_IN_WITH_APPLE";
+        private const string SsoConstantsHeader = "/Runtime/Plugins/iOS/ICSingleSignOnConstants.h";
+        private const string LineComment = "//";
+
+        public int callbackOrder => 0;
+
+        static SSOPreProcessBuild()
+        {
+            BuildPlayerWindow.RegisterBuildPlayerHandler(ValidateBuild);
+        }
+
+        private static void ValidateBuild(BuildPlayerOptions options)
+        {
+            options.options |= BuildOptions.StrictMode;
+            if (options.target == BuildTarget.iOS)
+            {
+                UpdateProviderDefines(true);
+            }
+
+            BuildPipeline.BuildPlayer(options);
+        }
+
+        public void OnPreprocessBuild(BuildReport report)
+        {
+            if (report.summary.platform == BuildTarget.iOS)
+            {
+                UpdateProviderDefines(false);
+            }
+        }
+
+        private static void UpdateProviderDefines(bool startFromUI)
+        {
+            var constantsHeaderFile = GetPackageRootPath() + SsoConstantsHeader;
+
+            if (!File.Exists(constantsHeaderFile))
+            {
+                var message =
+                    $"File {constantsHeaderFile} not found. Please make sure you build native ios before build unity.";
+
+                ReportError(startFromUI, message);
+            }
+
+            SetAppleProviderDefine(constantsHeaderFile, startFromUI);
+            SetGoogleProviderDefine(constantsHeaderFile, startFromUI);
+        }
+
+        private static string GetPackageRootPath()
+        {
+            var guid = AssetDatabase.FindAssets($"t:Script {nameof(SSOPreProcessBuild)}");
+            var path = AssetDatabase.GUIDToAssetPath(guid[0]);
+            return Path.Combine(Path.GetDirectoryName(path) ?? string.Empty, "..");
+        }
+
+        private static void SetGoogleProviderDefine(string constantsHeaderFile, bool startFromUi)
+        {
+            var defineFound = SetProviderDefine(constantsHeaderFile, DefineSignInWithGoogle,
+                false);
+            if (!defineFound)
+            {
+                ReportError(startFromUi,
+                    $"define {DefineSignInWithGoogle} not found in file {constantsHeaderFile}. Please make sure you build native code in xcode before building the app");
+            }
+        }
+
+        private static void SetAppleProviderDefine(string constantsHeaderFile, bool startFromUi)
+        {
+            var defineFound = SetProviderDefine(constantsHeaderFile, DefineSignInWithApple,
+                true);
+            if (!defineFound)
+            {
+                ReportError(startFromUi,
+                    $"define {DefineSignInWithApple} not found in file {constantsHeaderFile}. Please make sure you build native code in xcode before building the app");
+            }
+        }
+
+        private static bool SetProviderDefine(string serviceMFile, string define, bool isActive)
+        {
+            var lines = File.ReadAllLines(serviceMFile);
+            var writeLines = new string[lines.Length];
+            var defineFound = false;
+            for (var index = 0; index < lines.Length; index++)
+            {
+                string line = lines[index];
+                if (line.Contains(define))
+                {
+                    if (isActive)
+                    {
+                        line = define;
+                    }
+                    else
+                    {
+                        line = LineComment + define;
+                    }
+
+                    defineFound = true;
+                }
+
+                writeLines[index] = line;
+            }
+
+            File.WriteAllLines(serviceMFile, writeLines);
+            return defineFound;
+        }
+
+        private static void ReportError(bool startFromUi, string message)
+        {
+            if (startFromUi)
+            {
+                throw new BuildPlayerWindow.BuildMethodException(message);
+            }
+            else
+            {
+                throw new BuildFailedException(message);
+            }
+        }
+    }
+}
---
+++ Runtime/Plugins/iOS/ICSingleSignOnConstants.h
@@ -0,0 +1,10 @@
+#ifndef ICSingleSignOnConstants_h
+#define ICSingleSignOnConstants_h
+
+#define LOGGER_SSO_TAG @"SSO_Native"
+#define LOGGER_SSO_ENUM_NAME @"Com.Test.Core.Frontend.Logger.LoggerSSO"
+// This lines will be set by Unity PreBuildProcess - do not remove
+//#define SIGN_IN_WITH_GOOGLE
+// This lines will be set by Unity PreBuildProcess - do not remove
+#define SIGN_IN_WITH_APPLE
+#endif /* ICSingleSignOnConstants_h */
